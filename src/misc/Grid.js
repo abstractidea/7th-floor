affd732e1a94cf8e00a1acf138b002864317c051
Jeffrey Yu <jyu192@gmail.com>
2013-06-05 23:35:53 -0700

    Created Grid class

---
+++ src/misc/Grid.js
@@ -0,0 +1,54 @@
+(function (window) {
+
+	function Grid() {
+		this.initialize();
+	}
+
+	var p = Grid.prototype = new createjs.Container();
+
+// public properties:
+	Grid.WIDTH = 7;
+	Grid.HEIGHT = 14;
+
+// public properties:
+	p.color;
+	p.gridBody;
+
+// constructor:
+	p.Container_initialize = p.initialize;	//unique to avoid overiding base class
+
+	p.initialize = function () {
+		this.Container_initialize();
+
+		this.gridBody = new createjs.Shape();
+		this.color = "#1aafff";
+
+		this.addChild(this.blockBody);
+
+		this.makeShape();
+		
+	}
+
+// public methods:
+	p.makeShape = function () {
+		//draw square outline for body
+		var g = this.gridBody.graphics;
+		g.clear();
+		g.beginFill(this.color);
+
+		g.moveTo(0, 0);	//top-left
+		g.lineTo(Grid.WIDTH * Block.WIDTH, 0);	//top-right
+		g.lineTo(Grid.WIDTH * Block.WIDTH, Grid.HEIGHT * Block.HEIGHT);	//bottom-right
+		g.lineTo(0, Grid.HEIGHT * Block.HEIGHT);	//bottom-left
+		g.closePath(); //top-left
+
+	}
+
+	p.tick = function (event) {
+		//tick event
+				
+	}
+
+	window.Grid = Grid;
+
+}(window));
